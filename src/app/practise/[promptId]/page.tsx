bc64b10191f77f99930d391d01bb02c339a05e8a
skilledsantosh <santoshkunwar@skillprompt.com>
2024-06-22 20:39:41 +0545

    debug 5

---
+++ src/app/practise/[promptId]/page.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Page({ params }: { params: { promptId: string } }) {
+  return <div>page</div>;
+}
--- src/app/practise/[prompt_id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = ({ prompt_id }: { prompt_id: string }) => {
-  return <div>page</div>;
-};
-
-export default page;
