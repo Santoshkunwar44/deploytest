dae805a462db7ca6b4eb7ac78d462f9a4a4e2025
skilledsantosh <santoshkunwar@skillprompt.com>
2024-06-22 19:01:43 +0545

    deploy done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -73,7 +74,9 @@ export default function Home() {
             Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
-
+        <Link href={`/practise/2345`}>
+          <button>practise page</button>
+        </Link>
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
---
+++ src/app/practise/[prompt_id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>the dynamic page </div>;
+};
+
+export default page;
